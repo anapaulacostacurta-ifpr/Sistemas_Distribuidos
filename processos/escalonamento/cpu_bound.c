540547f0bdfb5394f631209e8ff43fc0848af90b
anapaulacostacurta-ifpr <anapaula.costacurta@ifpr.edu.br>
2024-08-13 21:07:52 -0300

    Update cpu_bound.c

--- processos/escalonamento/cpu_bound.c
+++ processos/escalonamento/cpu_bound.c
@@ -4,7 +4,8 @@
 
 int main() {
     volatile long long i;
-    for (i = 0; i < 1000000000LL; i++);
+    //for (i = 0; i < 1000000000LL; i++);
+    while(1);
     printf("Processo de ID %d terminou a execução.\n", getpid());
     return 0;
 }
