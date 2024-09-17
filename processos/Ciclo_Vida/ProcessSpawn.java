15cfa9b2f7cb7728c2140871542990e48b341808
anapaulacostacurta-ifpr <anapaula.costacurta@ifpr.edu.br>
2024-09-16 21:48:12 -0300

    Update ProcessSpawn.java

--- processos/Ciclo_Vida/ProcessSpawn.java
+++ processos/Ciclo_Vida/ProcessSpawn.java
@@ -5,6 +5,7 @@ public class ProcessSpawn {
     // Método para criar e executar um processo filho
     private static Process spawn(String program, String[] argList) throws IOException {
         ProcessBuilder processBuilder = new ProcessBuilder(argList);
+        System.out.println("Entrou no método Spawn");
         // Define o programa a ser executado
         processBuilder.command(program);
         return processBuilder.start();
